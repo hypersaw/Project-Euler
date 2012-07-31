a1cbb7a2ed840810e9389b785ca95e979d6c7e55
Andrew Long <along27@uic.edu>
2012-07-31 15:04:38 -0400

    Added global variable to dynamically change the largest product possible, but forgot to actually implement it. Newest revision reflects those changes, now less hard-coded.

--- euler4.c
+++ euler4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int largestPalindrome = 0;
 static int largestProduct = 6;	// Largest product (digit count)  of two 3-digit values
@@ -8,38 +9,34 @@ int main(int argc, char** argv){
 	int digit1 = 999;
 	int digit2 = 999;
 	int palindrome = 0;
-	char palindromeString[6];
+	char palindromeString[largestProduct];
 	int notPalindrome = 1;
+	int iterator;
+	int startPoint;
 
 	while(digit1 > 99 && notPalindrome){
 		while(digit2 > 99){
 			palindrome = digit1 * digit2;
 			// Extract palindrome
-			palindromeString[5] = palindrome % 10;
-			palindromeString[4] = palindrome % 100 / 10;
-			palindromeString[3] = palindrome % 1000 / 100;
-			palindromeString[2] = palindrome % 10000 / 1000;
-			palindromeString[1] = palindrome % 100000 / 10000;
-			palindromeString[0] = palindrome % 1000000 / 100000;
+			for(iterator = 1; iterator <= largestProduct; ++iterator){
+				palindromeString[largestProduct - iterator] = palindrome % (int)pow(10,iterator) / (int)pow(10,iterator-1);
+			}
 
-			int i = 0;
-			int startPoint = 0;
-			while(i < 6){
-				if(palindromeString[i] == 0){
-					++i;
-				}
-				else{
-					startPoint = i;
-					i = 6;
+			iterator = 0;
+			startPoint = 0;
+			for(iterator; iterator < largestProduct; ++iterator){
+				if(palindromeString[iterator] != 0){
+					startPoint = iterator;
+					iterator = largestProduct;
 				}
 			}
 
-			i = (6 - startPoint) / 2;
+			iterator = (largestProduct - startPoint) / 2;
 			notPalindrome = 0;
-			for(startPoint; startPoint < i; ++startPoint){
-				if(palindromeString[startPoint] != palindromeString[5 - startPoint]){
+			for(startPoint; startPoint < iterator; ++startPoint){
+				if(palindromeString[startPoint] != palindromeString[(largestProduct - 1) - startPoint]){
 					notPalindrome = 1;
-					startPoint = i;
+					startPoint = iterator;
 				}
 			}
 
